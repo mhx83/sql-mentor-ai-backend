fcd9a389a6de33e1e114fc9e1ad438b4590e0392
Zinan Zhang <126738864+harrryzhang1250@users.noreply.github.com>
2025-03-06 14:02:05 -0800

    Migrate Quizzes, Questions, Attempts

--- src/Answers/model.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import schema from "./schema.js";
-
-const model = mongoose.model("AnswerModel", schema);
-
-export default model;
--- src/Attempts/model.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import schema from "./schema.js";
-
-const model = mongoose.model("AttemptModel", schema);
-
-export default model;
